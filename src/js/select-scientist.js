ddf1a991ef6746ba68400d955031ffe1d3445a12
Stanislav Trunov <107206323+stanlettee@users.noreply.github.com>
2025-02-09 21:34:49 +0000

    scientist

--- src/js/select-scientist.js
+++ src/js/select-scientist.js
@@ -1,27 +1,22 @@
-// const overlay1 = document.querySelector('#overlay1')
-// const overlay2 = document.querySelector('#overlay2')
-// const overlay3 = document.querySelector('#overlay3')
-// const overlay4 = document.querySelector('#overlay4')
-// const overlay5 = document.querySelector('#overlay5')
-// const overlay6 = document.querySelector('#overlay6')
-// const overlay7 = document.querySelector('#overlay77')
-// const overlay8 = document.querySelector('#overlay8')
-// const overlay9 = document.querySelector('#overlay9')
-// const overlay10 = document.querySelector('#overlay10')
-// const overlay11 = document.querySelector('#overlay11')
-// const overlay12 = document.querySelector('#overlay12')
-let overlays = document.querySelectorAll('.select-scientist__overlay')
+const overlays = document.querySelectorAll('.select-scientist__overlay')
 
-const button1 = document.querySelector('#button1')
-const button2 = document.querySelector('#button2')
-const button3 = document.querySelector('#button3')
-const button4 = document.querySelector('#button4')
-const button5 = document.querySelector('#button5')
-const button6 = document.querySelector('#button6')
-const button7 = document.querySelector('#button7')
-const button8 = document.querySelector('#button8')
-const button9 = document.querySelector('#button9')
-const button10 = document.querySelector('#button10')
+const nameSpans = document.querySelectorAll('.scientist-name')
+const surnameSpans = document.querySelectorAll('.scientist-surname')
+const bornSpans = document.querySelectorAll('.scientist-born')
+const deadSpans = document.querySelectorAll('.scientist-dead')
+
+const button1 = document.querySelector('#button1id')
+const button2 = document.querySelector('#button2id')
+const button3 = document.querySelector('#button3id')
+const button4 = document.querySelector('#button4id')
+const button5 = document.querySelector('#button5id')
+const button6 = document.querySelector('#button6id')
+const button7 = document.querySelector('#button7id')
+const button8 = document.querySelector('#button8id')
+const button9 = document.querySelector('#button9id')
+const button10 = document.querySelector('#button10id')
+
+console.log(button1)
 
 button10.addEventListener('click', () => {
     for (let overlay of overlays){
@@ -30,47 +25,148 @@ button10.addEventListener('click', () => {
 })
 
 button1.addEventListener('click', () => {
-    for (let i = 0; i < overlays.length; i++){
-        switch (i + 1) {
-            case 1: 
-                overlays[i].style.backgroundColor = 'transparent';
-                break;
-            case 2: 
-                overlays[i].style.backgroundColor = '#D9D9D9';
-                break;
-            case 3: 
-                overlays[i].style.backgroundColor = '#D9D9D9';
-                break;
-            case 4: 
-                overlays[i].style.backgroundColor = 'transparent';
-                break;
-            case 5: 
-                overlays[i].style.backgroundColor = '#D9D9D9';
-                break;
-            case 6: 
-                overlays[i].style.backgroundColor = '#D9D9D9';
-                break;
-            case 7: 
-                overlays[i].style.backgroundColor = 'transparent';
-                break;
-            case 8: 
-                overlays[i].style.backgroundColor = 'transparent';
-                break;
-            case 9: 
-                overlays[i].style.backgroundColor = 'transparent';
-                break;
-            case 10: 
-                overlays[i].style.backgroundColor = 'transparent';
-                break;
-            case 11: 
-                overlays[i].style.backgroundColor = 'transparent';
-                break;
-            case 12: 
-                overlays[i].style.backgroundColor = 'transparent';
-                break;
-            default:
-                break;
+    for (let bornSpan of bornSpans) {
+        const birthYear = Number(bornSpan.innerHTML);
+        const overlay = bornSpan.closest('.select-scientist__div-item').lastElementChild;
+        if ( birthYear < 1900 && birthYear > 1799) {
+            overlay.style.backgroundColor = 'transparent';
+        } else {
+            overlay.style.backgroundColor = 'rgb(217, 217, 217)';
         }
     }
+    
 });
 
+button2.addEventListener('click', () => {
+    for (let nameSpan of nameSpans) {
+        if (nameSpan.innerHTML === 'Albert'){
+            const overlay = nameSpan.closest('.select-scientist__div-item').lastElementChild;
+            overlay.style.backgroundColor = overlay.style.backgroundColor = 'transparent';
+        } else {
+            const overlay = nameSpan.closest('.select-scientist__div-item').lastElementChild;
+            overlay.style.backgroundColor = 'rgb(217, 217, 217)';
+        }
+    }
+    
+});
+
+button4.addEventListener('click', () => {
+    for (let surnameSpan of surnameSpans) {
+        if (surnameSpan.innerHTML.startsWith('C')){
+            const overlay = surnameSpan.closest('.select-scientist__div-item').lastElementChild;
+            overlay.style.backgroundColor = overlay.style.backgroundColor = 'transparent';
+        } else {
+            const overlay = surnameSpan.closest('.select-scientist__div-item').lastElementChild;
+            overlay.style.backgroundColor = 'rgb(217, 217, 217)';
+        }
+    }
+    
+});
+
+button6.addEventListener('click', () => {
+    for (let nameSpan of nameSpans) {
+        if (nameSpan.innerHTML.startsWith('A')){
+            const overlay = nameSpan.closest('.select-scientist__div-item').lastElementChild;
+            overlay.style.backgroundColor = overlay.style.backgroundColor = 'rgb(217, 217, 217)';
+        } else {
+            const overlay = nameSpan.closest('.select-scientist__div-item').lastElementChild;
+            overlay.style.backgroundColor = 'transparent';
+        }
+    } 
+});
+
+button7.addEventListener('click', () => {
+    for (let i = 0; i < bornSpans.length; i++) {
+        const birthYear = Number(bornSpans[i].innerHTML);
+        const overlay = bornSpan.closest('.select-scientist__div-item').lastElementChild;
+        if ( Number(bornSpans[i].innerHTML) > Number(bornSpans[i + 1].innerHTML)) {
+            overlay.style.backgroundColor = 'transparent';
+        } else {
+            overlay.style.backgroundColor = 'rgb(217, 217, 217)';
+        }
+    }
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// spanElement.closest('.select-scientist__div-item')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (let i = 0; i < overlays.length; i++){
+
+    //     switch (i + 1) {
+    //         case 1: 
+    //             overlays[i].style.backgroundColor = 'transparent';
+    //             break;
+    //         case 2: 
+    //             overlays[i].style.backgroundColor = '#D9D9D9';
+    //             break;
+    //         case 3: 
+    //             overlays[i].style.backgroundColor = '#D9D9D9';
+    //             break;
+    //         case 4: 
+    //             overlays[i].style.backgroundColor = 'transparent';
+    //             break;
+    //         case 5: 
+    //             overlays[i].style.backgroundColor = '#D9D9D9';
+    //             break;
+    //         case 6: 
+    //             overlays[i].style.backgroundColor = '#D9D9D9';
+    //             break;
+    //         case 7: 
+    //             overlays[i].style.backgroundColor = 'transparent';
+    //             break;
+    //         case 8: 
+    //             overlays[i].style.backgroundColor = 'transparent';
+    //             break;
+    //         case 9: 
+    //             overlays[i].style.backgroundColor = 'transparent';
+    //             break;
+    //         case 10: 
+    //             overlays[i].style.backgroundColor = 'transparent';
+    //             break;
+    //         case 11: 
+    //             overlays[i].style.backgroundColor = 'transparent';
+    //             break;
+    //         case 12: 
+    //             overlays[i].style.backgroundColor = 'transparent';
+    //             break;
+    //         default:
+    //             break;
+    //     }
+    // }
