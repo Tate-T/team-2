e5f3ff3e8b5e572448375d2bf22f2abb06c68cf5
stanlettee <107206323+stanlettee@users.noreply.github.com>
2025-02-16 16:03:57 +0000

    Deploying to gh-pages from @ Tate-T/team-2@aff77005d2f4b2c2672b65f92fd94df7df7754a0 🚀

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import "./js/header.js";
-import "./js/check-age.js";
-import "./js/guess-number.js";
-import "./js/calculator.js";
-import "./js/three-numbers.js";
-import "./js/select-scientist.js";
-import "./js/our-team.js";
