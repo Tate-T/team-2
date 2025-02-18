4439bf5fabccc49fffca0a1beaec09f1d0332446
pipypupa <zong8729@gmail.com>
2025-02-18 17:06:25 +0200

    fix

---
+++ src/js/modal.js
@@ -0,0 +1,16 @@
+(() => {
+    const headerModal = {
+      closeModalBtn: document.querySelector("[data-close-modal]"),
+      closeModal: document.querySelector("[data-close-modal-button]"),
+      modal: document.querySelector("[data-modal]"),
+    };
+  
+    headerModal.closeModalBtn.addEventListener("click", toogleModal);
+    headerModal.closeModal.addEventListener("click", toogleModal);
+  
+    function toogleModal(e) {
+      e.preventDefault();
+      headerModal.modal.classList.toggle("is-hidden");
+    }
+  })();
+  
