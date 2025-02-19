4bff7c6ee6d40e83153013790500b7c81351c7cf
Stanislav Trunov <107206323+stanlettee@users.noreply.github.com>
2025-02-19 20:04:19 +0000

    img fix

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import "./js/guess-number.js";
 import "./js/calculator.js";
 import "./js/three-numbers.js";
 import "./js/dino.js";
+import "./js/football.js";
 import "./js/select-scientist.js";
 import "./js/our-team.js";
 import "./js/footer-modal.js";
