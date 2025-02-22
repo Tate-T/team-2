d6d838d50fa79108369dac415144d10bf2770b8a
Stanislav Trunov <107206323+stanlettee@users.noreply.github.com>
2025-02-22 23:10:35 +0000

    tic tac toe game and hovers

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import "./js/calculator.js";
 import "./js/three-numbers.js";
 import "./js/age-calculator.js";
 import "./js/dino.js";
+import "./js/tic-tac-toe.js";
 import "./js/football.js";
 import "./js/select-scientist.js";
 import "./js/our-team.js";
