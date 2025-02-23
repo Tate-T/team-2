4af5ee19310232db89be2bd4b1f1d23ba2809616
Stanislav Trunov <107206323+stanlettee@users.noreply.github.com>
2025-02-23 16:10:45 +0000

    our team

--- src/js/our-team.js
+++ src/js/our-team.js
@@ -5,39 +5,66 @@ const name1 = document.querySelector('.our-team__name')
 const image = document.querySelector('.our-team__image')
 const element1 = document.querySelector('#element1')
 const element2 = document.querySelector('#element2')
+const stas = document.querySelector('#stas')
+const andrey = document.querySelector('#andrey')
+let index = 1
+
+// button1.addEventListener("click", () => {
+//     if (image.src.endsWith("andrey.jpg")) {
+//         image.src = "./img/stas.jpg";
+//         name1.innerHTML = "Стас Трунов";
+//         text.innerHTML = "Зробив секції header, check-age, calculator, guess-number, select-scinetist, our-team, three-numbers, google dino";
+//         element2.style.width = '30px';
+//         element2.style.backgroundColor = '#797979'
+//         element1.style.width = '18px'
+//         element1.style.backgroundColor = '#D9D9D9'
+//     } else {
+//         image.src = "./img/andrey.jpg";
+//         name1.innerHTML = "Андрій Норик";
+//         text.innerHTML =
+//             "Зробив модалку, футер та секції age-calculator, rock-paper-scissors та football";
+//         element1.style.width = '30px';
+//         element1.style.backgroundColor = '#797979'
+//         element2.style.width = '18px'
+//         element2.style.backgroundColor = '#D9D9D9'
+//     }
+// });
+
+// button2.addEventListener("click", () => {
+//     if (image.src.endsWith("andrey.jpg")) {
+//         image.src = "./img/stas.jpg";
+//         name1.innerHTML = "Стас Трунов";
+//         text.innerHTML = "Зробив секції header, check-age, calculator, guess-number, select-scinetist, our-team, three-numbers, google dino";
+//         element2.style.width = '30px'
+//         element2.style.backgroundColor = '#797979'
+//         element1.style.width = '18px'
+//         element1.style.backgroundColor = '#D9D9D9'
+//     } else {
+//         image.src = "./img/andrey.jpg";
+//         name1.innerHTML = "Андрій Норик";
+//         text.innerHTML =
+//             "Зробив модалку, футер та секції age-calculator, rock-paper-scissors та football";
+//         element1.style.width = '30px';
+//         element1.style.backgroundColor = '#797979'
+//         element2.style.width = '18px'
+//         element2.style.backgroundColor = '#D9D9D9'
+//     }
+// });
 
 button1.addEventListener("click", () => {
-    if (image.src.endsWith("andrey.jpg")) {
-        image.src = "./img/stas.jpg";
+    if (index === 1) {
+        stas.style.display = 'inherit';
+        andrey.style.display = "none";
         name1.innerHTML = "Стас Трунов";
-        text.innerHTML = "Зробив секції header, check-age, calculator, guess-number, select-scinetist, our-team, three-numbers, google dino";
-        element2.style.width = '30px';
-        element2.style.backgroundColor = '#797979'
-        element1.style.width = '18px'
-        element1.style.backgroundColor = '#D9D9D9'
-    } else {
-        image.src = "./img/andrey.jpg";
-        name1.innerHTML = "Андрій Норик";
-        text.innerHTML =
-            "Зробив модалку, футер та секції age-calculator, rock-paper-scissors та football";
-        element1.style.width = '30px';
-        element1.style.backgroundColor = '#797979'
-        element2.style.width = '18px'
-        element2.style.backgroundColor = '#D9D9D9'
-    }
-});
-
-button2.addEventListener("click", () => {
-    if (image.src.endsWith("andrey.jpg")) {
-        image.src = "./img/stas.jpg";
-        name1.innerHTML = "Стас Трунов";
-        text.innerHTML = "Зробив секції header, check-age, calculator, guess-number, select-scinetist, our-team, three-numbers, google dino";
+        text.innerHTML = "Зробив секції header, check-age, calculator, guess-number, select-scinetist, our-team, three-numbers, google dino, та tic tac toe";
         element2.style.width = '30px'
         element2.style.backgroundColor = '#797979'
         element1.style.width = '18px'
         element1.style.backgroundColor = '#D9D9D9'
-    } else {
-        image.src = "./img/andrey.jpg";
+        index += 1
+    } else if (index === 2) {
+        andrey.style.display = "inherit";
+        stas.style.display = "none";
         name1.innerHTML = "Андрій Норик";
         text.innerHTML =
             "Зробив модалку, футер та секції age-calculator, rock-paper-scissors та football";
@@ -45,5 +72,35 @@ button2.addEventListener("click", () => {
         element1.style.backgroundColor = '#797979'
         element2.style.width = '18px'
         element2.style.backgroundColor = '#D9D9D9'
+        index -= 1
+    }
+    }
+);
+
+button2.addEventListener("click", () => {
+    if (index === 1) {
+        stas.style.display = 'inherit';
+        andrey.style.display = "none";
+        name1.innerHTML = "Стас Трунов";
+        text.innerHTML = "Зробив секції header, check-age, calculator, guess-number, select-scinetist, our-team, three-numbers, google dino, та tic tac toe";
+        element2.style.width = '30px'
+        element2.style.backgroundColor = '#797979'
+        element1.style.width = '18px'
+        element1.style.backgroundColor = '#D9D9D9'
+        index += 1
+    } else if (index === 2) {
+        andrey.style.display = "inherit";
+        stas.style.display = "none";
+        name1.innerHTML = "Андрій Норик";
+        text.innerHTML =
+            "Зробив модалку, футер та секції age-calculator, rock-paper-scissors та football";
+        element1.style.width = '30px';
+        element1.style.backgroundColor = '#797979'
+        element2.style.width = '18px'
+        element2.style.backgroundColor = '#D9D9D9'
+        index -= 1
     }
 });
+
+
+
